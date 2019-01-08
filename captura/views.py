c39b7fbef831530a5885600bfe23b7183d80b957
Gabriel <gabrielcasemiro68@gmail.com>
2019-01-08 18:54:31 -0200

    url

--- captura/views.py
+++ captura/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 import re
-import urllib2
+import urllib.request as urllib2
 
 # Create your views here.
 def home(request):
