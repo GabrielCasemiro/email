6a9521c46a0e074fb6fcdbe6dbfa075e7e817602
Gabriel <gabrielcasemiro68@gmail.com>
2019-01-08 14:53:03 -0200

    error

--- captura/views.py
+++ captura/views.py
@@ -13,14 +13,11 @@ def capturar(request):
 	if request.method == 'POST':
 		link = request.POST.get("link", '')
 		if link:
-			try:
-				f = urllib.urlopen(link)
-				s = f.read()
-				telefones = re.findall(r"\+\d{2}\s?0?\d{10}",s)
-				emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",s)
-				msg = "Link Válido"
-			except:
-				msg = "Link inválido."
-			
+			f = urllib.urlopen(link)
+			s = f.read()
+			telefones = re.findall(r"\+\d{2}\s?0?\d{10}",s)
+			emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",s)
+			msg = "Link Válido"
 		return render(request, 'index.html', {"link":link,"msg":msg,"emails":emails,"telefones":telefones})
-	return render(request, 'index.html')
+	else:
+		return render(request, 'index.html')
