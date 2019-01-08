f26c7c9f8bb9a5b65b8af15f349231fca5e5d0fc
Gabriel <gabrielcasemiro68@gmail.com>
2019-01-08 18:37:30 -0200

    2

--- captura/views.py
+++ captura/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-import urllib,re
-import urllib.request
+import re
+import urllib2
 
 # Create your views here.
 def home(request):
@@ -13,10 +13,20 @@ def capturar(request):
 	telefones = []
 
 	if request.method == 'POST':
+		hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 		link = request.POST.get("link", '')
 		if link:
-			with urllib.request.urlopen(link) as url:
-				s = url.read().decode('utf-8')
+			req = urllib2.Request(link, headers=hdr)
+			try:
+				page = urllib2.urlopen(req)
+			except urllib2.HTTPError:
+				pass
+			s = page.read().decode('utf-8')
 			
 			telefones = re.findall(r"\+\d{2}\s?0?\d{10}",s)
 			emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",s)
