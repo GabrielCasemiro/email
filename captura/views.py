f68a29adcf66656959827b94a898a812d67286ec
Gabriel <gabrielcasemiro68@gmail.com>
2019-01-08 14:38:56 -0200

    primeiro commit

---
+++ captura/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+import urllib,re
+# Create your views here.
+def home(request):
+	return render(request, 'index.html', {})
+def capturar(request):
+	emails = []
+	telefones = []
+
+	if request.method == 'POST':
+		link = request.POST.get("link", '')
+		if link:
+			try:
+				f = urllib.urlopen(link)
+				s = f.read()
+				telefones = re.findall(r"\+\d{2}\s?0?\d{10}",s)
+				emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",s)
+				msg = "Link Válido"
+			except:
+				msg = "Link inválido."
+			
+		return render(request, 'index.html', {"link":link,"msg":msg,"emails":emails,"telefones":telefones})
+	return render(request, 'index.html')
